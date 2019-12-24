915f40baeb518319c460c6f7dfa19c377cc09001
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2019-12-23 21:07:23 -0500

    Reestructurando Los conceptos de las clases

---
+++ Modules/Variables(String)/js/index.js
@@ -0,0 +1,25 @@
+let name = 'Daniel Fernando';
+let lastName = 'Yepez Vélez';
+let completeName;
+
+completeName = `${name.toLowerCase()} ${lastName.toLowerCase()}`;
+console.log(completeName);
+
+completeName = `${name.toUpperCase()} ${lastName.toUpperCase()}`;
+console.log(completeName);
+
+// /* Reto Strings */
+// let usuario = prompt('Señor usuario ingrese el nombre: ');
+// let ultimaLetra = usuario.charAt(usuario.length - 1);
+// console.log('Última Letra Nombre Usuario:', ultimaLetra);
+
+// /* =================================================== */
+
+// let inicioCadena = usuario.substring(3);
+// console.log(inicioCadena);
+
+// let segmentoCadena = usuario.substr(0, 4);
+// console.log(segmentoCadena);
+
+// let posicionCaracter = usuario.charAt(3);
+// console.log(posicionCaracter);
---
+++ Modules/Variables/js/index.js
@@ -0,0 +1,8 @@
+/* Variables */
+var gretting = 'Hello';
+let name = 'Daniel Fernando';
+var lastName = 'Yepez Vélez';
+let age = 26;
+
+console.log(gretting + ' ' + name + ' ' + lastName);
+console.log(`I am ${age} years old`);
