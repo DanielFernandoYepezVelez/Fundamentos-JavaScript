c9f182db4c4a1b07609137cd92c04778011d43c5
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2020-03-21 01:53:55 -0500

    Ajustes finales de local y session storage

--- 11-LocalStorageInformation/js/index.js
+++ 11-LocalStorageInformation/js/index.js
@@ -2,8 +2,57 @@
 /* Nos sirve para añadir cierta informacion
 en los navegadores de nuestros usuarios 
 y aunque se cierre el navegador la informacion
-sigue de forma persistente */
+sigue de forma persistente.
+LOCAL STORAGE: Solo Recibe Strings
+NO OLVIDAR EL CONDICIONAL PARA VER SI EXISTE EN EL LOCAL STORAGE*/
 
-/* AGREGAR AL LOCAL STORAGE */
 /* el formato es tipo JSON, es decir, toma una llave y un valor */
+/* AGREGAR AL LOCAL STORAGE */
 localStorage.setItem('nombre', 'Daniel');
+
+/* OBTENER UN VALOR DEL LOCAL STORAGE */
+const name = localStorage.getItem('nombre');
+console.log(name);
+
+/* REMOVER DEL LOCAL STORAGE */
+// localStorage.removeItem('nombre')
+
+/* ELIMINAR TODO LO EXISTENTE EN EL LOCAL STORAGE */
+// localStorage.clear();
+
+/* AQUI VAMOS A AGREGAR UN OBJETO JSON AL LOCAL STORAGE*/
+localStorageAdd();
+
+function localStorageAdd() {
+    let persona = {
+            name: 'Daniel Fernando',
+            lastName: 'Yepez Velez',
+            age: '27 años',
+            coords: {
+                latitude: 100,
+                altura: 1000
+            }
+        }
+        /* Local Storage solo recibe strings, por eso debo aplicar dicha función */
+    localStorage.setItem('object', JSON.stringify(persona));
+}
+
+/* AQUI VAMOS A OBTENER UN OBJETO JSON DEL LOCAL STORAGE*/
+localStorageGET();
+
+function localStorageGET() {
+    let object = localStorage.getItem('object');
+    console.log(object);
+
+    /* Lo parseo para obtener nuevamente un JSON y poder aplicar sus metodos y propiedades */
+    object = JSON.parse(localStorage.getItem('object'));
+    console.log(object);
+}
+
+/* AQUI VAMOS A ELIMINAR UN OBJETO JSON DEL LOCAL STORAGE*/
+/* localStorageREMOVE();
+
+function localStorageREMOVE() {
+    let object = localStorage.removeItem('object');
+    console.log(object);
+} */
--- 12-SessionStorageInformation/js/index.js
+++ 12-SessionStorageInformation/js/index.js
@@ -3,8 +3,58 @@
 en los navegadores de nuestros usuarios como el
 localStorage, la diferencia radica en que 
 al cerrar el navegador todo lo que exista en el
-SessionStorage Desaparece */
+SessionStorage Desaparece 
+SESSION STORAGE: Solo Recibe Strings.
+NO OLVIDAR EL CONDICIONAL PARA VER SI EXISTE EN EL SESSION STORAGE*/
 
-/* AGREGAR AL SESIONSTORAGE */
+/* AGREGAR AL SESSION STORAGE */
 /* el formato es tipo JSON, es decir, toma una llave y un valor */
 sessionStorage.setItem('apellidos', 'Yepez Velez');
+
+/* OBTENER VALORES DEL SESSION STORAGE */
+const lastName = sessionStorage.getItem('apellidos');
+console.log(lastName);
+
+/* REMOVER DEL SESSION STORAGE */
+// sessionStorage.removeItem('apellidos');
+
+/* ELIMINAR TODO LO DEL SESSION STORAGE */
+// sessionStorage.clear();
+
+/* AQUI VAMOS A AGREGAR UN OBJETO JSON AL SESSION STORAGE*/
+sessionStorageAdd();
+
+function sessionStorageAdd() {
+    let persona = {
+            name: 'Daniel Fernando',
+            lastName: 'Yepez Velez',
+            age: '27 años',
+            coords: {
+                latitude: 100,
+                altura: 1000
+            }
+        }
+        /* Session Storage solo recibe strings, por eso debo aplicar dicha función */
+    sessionStorage.setItem('object', JSON.stringify(persona));
+}
+
+/* AQUI VAMOS A OBTENER UN OBJETO JSON DEL SESSION STORAGE*/
+sessionStorageGET();
+
+function sessionStorageGET() {
+    let object = sessionStorage.getItem('object');
+    console.log(object);
+
+    /* Lo parseo para obtener nuevamente un JSON y poder aplicar sus metodos y propiedades */
+    object = JSON.parse(sessionStorage.getItem('object'));
+    console.log(object);
+
+}
+
+/* AQUI VAMOS A ELIMINAR UN OBJETO JSON DEL SESSION STORAGE*/
+/* sessionStorageREMOVE();
+
+function sessionStorageREMOVE() {
+    let object = sessionStorage.removeItem('object');
+    console.log(object);
+} */
