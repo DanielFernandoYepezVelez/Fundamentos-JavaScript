5b4a013d78bee8e7e253a31e7df76c3b3a8474d2
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2020-04-11 08:18:56 -0500

    Adjuntando Documentación De API's y REST-API

---
+++ 15-ServicesRestInformation/02-RestAPI'sAJAX/app.js
@@ -0,0 +1,28 @@
+/* Accediendo Al DOM */
+const cargarPost = document.querySelector('#cargar');
+
+cargarPost.addEventListener('click', cargarAPI);
+
+function cargarAPI() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
+
+    xhr.onload = function() {
+        if (this.status === 200) {
+            const response = JSON.parse(this.responseText);
+
+            let contenido = ''
+            response.forEach(function(post) {
+                contenido += `
+                  <h3>${post.title}</h3>
+                  <p>${post.body}</p>
+                `;
+            })
+
+            document.getElementById('listado').innerHTML = contenido;
+        }
+    };
+
+    xhr.send();
+}
